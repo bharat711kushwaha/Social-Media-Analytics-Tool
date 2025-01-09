a4b6ee628238f10a49dbc646ab4df0915cc54666
vermarjun <vermarjun26@gmail.com>
2025-01-09 22:13:47 +0530

    Chakra Ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <Button>
+        Click Me  
+      </Button>     
     </>
   )
 }
